77e9d5a5cb6fcbcbc0a7db21687dcb4e5538a31d
Jori Peterson <joripeterson@gmail.com>
2019-10-20 16:26:41 -0600

    Add show action for profile

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,9 +1,17 @@
 class Api::V1::UsersController < ApplicationController
 
+  def show
+    user = current_user
+    render json: user, status: 200
+  end
+
   def update
     user = current_user
-    user.update(user_params)
-    render json: user, status: 201
+    if user.update(user_params)
+      render json: user, status: 200
+    else
+      render json: 404
+    end    
   end
 
 private
