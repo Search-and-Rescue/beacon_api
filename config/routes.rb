bdde89c1485507b5821f3b489d8854e2e1e2860b
Jori Peterson <joripeterson@gmail.com>
2019-10-20 17:32:27 -0600

    Add route for new profile

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get '/profile', to: 'users#show'
+      post'/profile/new', to: 'users#create'
       patch '/profile/edit', to: 'users#update'
     end
   end
