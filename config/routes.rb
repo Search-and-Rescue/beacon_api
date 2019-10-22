770c740d2a7fb912bed53b044ec9243f0bb413b5
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-10-21 19:07:34 -0600

    Adjust graphiql path and remove old REST routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   if Rails.env.development?
-    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
+    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "graphql#execute"
   end
   post "/graphql", to: "graphql#execute"
-
-  # namespace :api do
-  #   namespace :v1 do
-  #     get '/profile', to: 'users#show'
-  #     post'/profile/new', to: 'users#create'
-  #     patch '/profile/edit', to: 'users#update'
-  #   end
-  # end
 end
