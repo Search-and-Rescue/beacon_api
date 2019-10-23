92d34c6b5a32d5370e4aa60428666896ae72d444
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-10-23 15:23:56 -0600

    Update user model test to reflect change to zip code validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,8 +14,6 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of(:state) }
     it { should validate_length_of(:state) }
     it { should validate_presence_of(:zip) }
-    it { should validate_length_of(:zip) }
-    it { should validate_numericality_of(:zip) }
   end
 
   describe User do
