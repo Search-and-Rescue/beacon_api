183d2180dfce03f1dcbde92e8ec51b51477c4f10
Jori Peterson <joripeterson@gmail.com>
2019-10-22 21:08:55 -0600

    Uncomment out fields on trip type

--- app/graphql/types/trip_type.rb
+++ app/graphql/types/trip_type.rb
@@ -1,6 +1,6 @@
 module Types
   class TripType < Types::BaseObject
-    # field :id, Integer, null: false
+    field :id, Integer, null: false
     field :name, String, null: false
     field :starting_point, String, null: false
     field :ending_point, String, null: false
@@ -11,6 +11,5 @@ module Types
     # field :notification_date, Types::DateTimeType, null: true
     # field :notification_time, Types::DateTimeType, null: true
     field :traveling_companions, Integer, null: true
-    # field :user, Types::UserType, null: false
   end
 end
