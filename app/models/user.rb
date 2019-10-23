193ef72af810f279f46a884727228255c1468e0a
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-10-23 11:50:31 -0600

    Allow zip code to have hyphens

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
 
   validates :email, uniqueness: true, presence: true
   validates :state, presence: true, length: { is: 2 }
-  validates :zip, presence: true, numericality: true, length: { is: 5 }
+  validates :zip, presence: true
   validates_presence_of :name, :address, :city
 
   enum experience_level: ["beginner", "moderate", "advanced"]
