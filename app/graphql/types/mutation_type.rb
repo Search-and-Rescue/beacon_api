969b1a3579a53960a297c5dd3031f3aa927d98c2
Jori Peterson <joripeterson@gmail.com>
2019-10-23 14:18:55 -0600

    Add mutation types for trip create and update

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,5 +2,7 @@ module Types
   class MutationType < Types::BaseObject
     field :createUser, mutation: Mutations::CreateUser
     field :updateUser, mutation: Mutations::UpdateUser
+    field :createTrip, mutation: Mutations::CreateTrip
+    field :updateTrip, mutation: Mutations::UpdateTrip
   end
 end
