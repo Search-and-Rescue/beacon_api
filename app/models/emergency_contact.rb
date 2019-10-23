969b7172c85b394a912e7cbb509f2e2869a64424
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-10-22 22:31:28 -0600

    Add validations to emergency contact

--- app/models/emergency_contact.rb
+++ app/models/emergency_contact.rb
@@ -1,3 +1,7 @@
 class EmergencyContact < ApplicationRecord
   belongs_to :user
+
+  validates :name, uniqueness: false, presence: true
+  validates :phone, presence: true
+  validates :email, presence: true
 end
