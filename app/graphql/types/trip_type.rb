6822530c908b6845ae6bc9c6136c30f745cb77bc
Jori Peterson <joripeterson@gmail.com>
2019-10-23 14:17:44 -0600

    Define trip type

--- app/graphql/types/trip_type.rb
+++ app/graphql/types/trip_type.rb
@@ -1,15 +1,15 @@
 module Types
   class TripType < Types::BaseObject
-    field :id, Integer, null: false
+    field :id, ID, null: false
     field :name, String, null: false
     field :starting_point, String, null: false
     field :ending_point, String, null: false
-    # field :start_date, Types::DateTimeType, null: true
-    # field :start_time, Types::DateTimeType, null: true
-    # field :end_date, Types::DateTimeType, null: true
-    # field :end_time, Types::DateTimeType, null: true
-    # field :notification_date, Types::DateTimeType, null: true
-    # field :notification_time, Types::DateTimeType, null: true
+    field :start_date, String, null: true
+    field :start_time, String, null: true
+    field :end_date, String, null: true
+    field :end_time, String, null: true
+    field :notification_date, String, null: true
+    field :notification_time, String, null: true
     field :traveling_companions, Integer, null: true
     field :user, Types::UserType, null: false
   end
