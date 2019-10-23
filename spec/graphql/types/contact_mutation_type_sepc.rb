bc768a384d70dfec458e7eef017fd43e9328c801
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-10-23 15:21:31 -0600

    Create tests for endpoint requests and queries

---
+++ spec/graphql/types/contact_mutation_type_sepc.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+
+RSpec.describe Types::QueryType do
+  describe 'emergency_contacts' do
+    it "should create a emergency contact for a user" do
+      user = create(:user)
+      mutation = (
+        %(mutation{
+          createContact(input: {
+            userId: #{user.id}
+            name: "Julie"
+            phone: "232-233-3232"
+            email: "julie@gmail.com"
+          }) {
+            clientMutationId
+          }
+        })
+      )
+
+      SearchAndRescueApiSchema.execute(mutation).as_json
+      expect(EmergencyContact.last.name).to eq("Julie")
+      expect(EmergencyContact.last.phone).to eq("232-233-3232")
+      expect(EmergencyContact.last.email).to eq("julie@gmail.com")
+    end
+
+    it "should update a contact" do
+      user = create(:user)
+      contact = create(:emergency_contact, user_id: user.id)
+      mutation = (
+        %(mutation{
+          updateContact(input: {
+            id: #{contact.id}
+            name: "Julie"
+            phone: "232-233-3232"
+            email: "julie@gmail.com"
+          }) {
+            clientMutationId
+          }
+        })
+      )
+
+      SearchAndRescueApiSchema.execute(mutation).as_json
+      expect(EmergencyContact.last.name).to eq("Julie")
+      expect(EmergencyContact.last.phone).to eq("232-233-3232")
+      expect(EmergencyContact.last.email).to eq("julie@gmail.com")
+    end
+  end
+end
