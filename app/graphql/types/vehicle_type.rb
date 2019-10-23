faaa990c44a422b36f3206a65bb57b7a2bf868dd
Jori Peterson <joripeterson@gmail.com>
2019-10-23 17:55:25 -0600

    Create vehicle type

---
+++ app/graphql/types/vehicle_type.rb
@@ -0,0 +1,11 @@
+module Types
+  class VehicleType < Types::BaseObject
+    field :id, ID, null: false
+    field :make, String, null: false
+    field :model, String, null: false
+    field :year, Integer, null: false
+    field :color, String, null: true
+    field :license_plate, String, null: true
+    field :user, Types::UserType, null: false
+  end
+end
