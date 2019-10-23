d79b8ec21ba18fbbc7817138f8a0708ba0022715
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-10-23 15:26:00 -0600

    Adjust phone number being required

--- app/graphql/types/emergency_contact_type.rb
+++ app/graphql/types/emergency_contact_type.rb
@@ -2,7 +2,7 @@ module Types
   class EmergencyContactType < Types::BaseObject
     field :id, ID, null: false
     field :name, String, null: false
-    field :phone, String, null: true
+    field :phone, String, null: false
     field :email, String, null: false
 
     field :user, Types::UserType, null: false
