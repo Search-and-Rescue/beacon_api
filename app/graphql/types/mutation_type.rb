4b8dff81feb7a0c1d54d1c6490b053662feab404
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-10-23 15:26:28 -0600

    Create mutations for contact creation and updating and add to mutation types

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,5 +1,7 @@
 module Types
   class MutationType < Types::BaseObject
+    field :createContact, mutation: Mutations::CreateContact
+    field :updateContact, mutation: Mutations::UpdateContact
     field :createUser, mutation: Mutations::CreateUser
     field :updateUser, mutation: Mutations::UpdateUser
   end
