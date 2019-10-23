9047cdf2301812a07906395e62870cffbacbbf62
Jori Peterson <joripeterson@gmail.com>
2019-10-23 14:17:22 -0600

    Remove test from broader query type spec

--- spec/graphql/types/query_type_spec.rb
+++ spec/graphql/types/query_type_spec.rb
@@ -34,35 +34,4 @@ RSpec.describe Types::QueryType do
       expect(results['name']).to eq(user.name)
     end
   end
-
-  describe "trips" do
-    it "should return all a user's trips" do
-      user = create(:user)
-      create_pair(:trip, user_id: user.id)
-      query = (
-        %(query {
-          user(id: #{user.id}) {
-            trips {
-              name
-            }
-          }
-        })
-      )
-      trips = SearchAndRescueApiSchema.execute(query).as_json['data']['user']['trips']
-      expect(trips.length).to eq(2)
-    end
-
-    it "should return a single trip" do
-      trip_1 = create(:trip, name: "trip_1")
-      query = (
-        %(query {
-          trip(id: #{trip_1.id}) {
-            name
-          }
-        })
-      )
-      trip = SearchAndRescueApiSchema.execute(query).as_json['data']['trip']
-      expect(trip['name']).to eq(trip_1.name)
-    end
-  end
 end
