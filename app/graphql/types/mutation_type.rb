7b06cf3cb76c89d5fadfe0330db945abafe9c70c
Jori Peterson <joripeterson@gmail.com>
2019-10-23 18:37:57 -0600

    Add createVehicle to mutations

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -4,5 +4,6 @@ module Types
     field :updateUser, mutation: Mutations::UpdateUser
     field :createTrip, mutation: Mutations::CreateTrip
     field :updateTrip, mutation: Mutations::UpdateTrip
+    field :createVehicle, mutation: Mutations::CreateVehicle
   end
 end
