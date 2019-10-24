8cad0572b50d7e0ce2a74415656b16e68274e64e
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-10-24 11:39:59 -0600

    Create model tests for has_many association between trips and emergency_contacts

--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Trip, type: :model do
   describe 'Relationships' do
     it { should belong_to :user }
+    it { should have_many(:emergency_contacts).through(:trip_contacts) }
   end
 
   describe 'Validations' do
