2e5fbf549a17429418197d87f22ca3a7657d46d2
Jori Peterson <joripeterson@gmail.com>
2019-10-24 14:15:03 -0600

    Add dependent destroy to user associations

--- app/models/gear.rb
+++ app/models/gear.rb
@@ -1,3 +1,3 @@
 class Gear < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, dependent: :destroy
 end
--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,3 +1,3 @@
 class Vehicle < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, dependent: :destroy
 end
