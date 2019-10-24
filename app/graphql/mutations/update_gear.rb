92cc9b5d2b9f25e6b51fe58c532a676d659da715
Jori Peterson <joripeterson@gmail.com>
2019-10-24 13:59:09 -0600

    Update gear mutation

---
+++ app/graphql/mutations/update_gear.rb
@@ -0,0 +1,23 @@
+class Mutations::UpdateGear < Mutations::BaseMutation
+  argument :id, ID, required: true
+  argument :user_id, ID, required: true
+  argument :item_name, String, required: false
+
+  field :gear, Types::GearType, null: true
+  field :errors, [String], null: false
+
+  def resolve(**attributes)
+    gear = Gear.find(attributes[:id])
+    if gear.update!(attributes)
+      {
+        gear: gear,
+        error: []
+      }
+    else
+      {
+        gear: nil,
+        errors: gear.errors.full_messages
+      }
+    end
+  end
+end
