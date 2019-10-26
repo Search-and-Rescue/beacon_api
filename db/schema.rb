3666765c82febc160751275fa02fea0139a7eeaf
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-10-26 14:12:36 -0600

    Create migration to add activity type to trips

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_26_195033) do
+ActiveRecord::Schema.define(version: 2019_10_26_200222) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -78,6 +78,7 @@ ActiveRecord::Schema.define(version: 2019_10_26_195033) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "activity_type"
     t.index ["user_id"], name: "index_trips_on_user_id"
   end
 
