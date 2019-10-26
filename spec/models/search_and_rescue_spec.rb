7a5688ef6b93550f8507c6ff87b9c735fdc75103
Jori Peterson <joripeterson@gmail.com>
2019-10-26 12:56:21 -0600

    Add spec and factory for search and rescue model

---
+++ spec/models/search_and_rescue_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe SearchAndRescue, type: :model do
+  describe 'Validations' do
+    it { should validate_presence_of :team_name}
+    it { should validate_presence_of :county}
+    it { should validate_presence_of :contact}
+    it { should validate_presence_of :contact_number}
+    it { should validate_presence_of :state}
+  end
+end
