de3e10798480aa38e2219a4fdf7bd392d6687b16
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-10-25 22:22:24 -0600

    Update tests for new trip_gear associations

--- spec/models/gear_spec.rb
+++ spec/models/gear_spec.rb
@@ -3,5 +3,6 @@ require 'rails_helper'
 RSpec.describe Gear, type: :model do
   describe 'Relationships' do
     it { should belong_to :user }
+    it { should have_many(:trips).through(:trip_gears) }
   end
 end
--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe Trip, type: :model do
   describe 'Relationships' do
     it { should belong_to :user }
     it { should have_many(:emergency_contacts).through(:trip_contacts) }
+    it { should have_many(:gears).through(:trip_gears) }
   end
 
   describe 'Validations' do
