f82c19bf535287a40c6732d5539ecd0d0ffba09e
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-10-26 14:01:37 -0600

    Add description to gear type

--- app/graphql/mutations/create_gear.rb
+++ app/graphql/mutations/create_gear.rb
@@ -1,6 +1,7 @@
 class Mutations::CreateGear < Mutations::BaseMutation
   argument :user_id, ID, required: true
   argument :item_name, String, required: true
+  argument :description, String, required: false
 
   field :gear, Types::GearType, null: true
   field :errors, [String], null: false
