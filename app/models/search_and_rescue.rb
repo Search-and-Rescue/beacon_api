88b5ecaf82d4accab6936cba20d4b73d6e7d55b7
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-10-28 18:16:50 -0600

    Update SAR model to geocode by city, state

--- app/models/search_and_rescue.rb
+++ app/models/search_and_rescue.rb
@@ -3,5 +3,13 @@ class SearchAndRescue < ApplicationRecord
   validates_presence_of :county
   validates_presence_of :contact
   validates_presence_of :contact_number
+  validates_presence_of :city
   validates_presence_of :state
+
+  geocoded_by :address
+  after_validation :geocode
+
+  def address
+    [city, state, 'US'].compact.join(', ')
+  end
 end
