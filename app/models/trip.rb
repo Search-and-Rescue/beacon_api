17057412803125d8129f9e5bf48f2fb6d385bcb0
Jori Peterson <joripeterson@gmail.com>
2019-10-29 19:11:45 -0600

    Add dependent destroy to trip_gears

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,7 +2,7 @@ class Trip < ApplicationRecord
   belongs_to :user
   has_many :trip_contacts, dependent: :destroy
   has_many :emergency_contacts, through: :trip_contacts
-  has_many :trip_gears
+  has_many :trip_gears, dependent: :destroy
   has_many :gears, through: :trip_gears
 
   validates_presence_of :name
