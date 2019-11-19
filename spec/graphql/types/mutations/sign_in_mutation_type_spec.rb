caf1cd5202ef6f6c4d827fe10144f140caa232d7
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-11-19 12:07:15 -0700

    Fix sign in test to perform the sign in mutation

--- spec/graphql/types/mutations/sign_in_mutation_type_spec.rb
+++ spec/graphql/types/mutations/sign_in_mutation_type_spec.rb
@@ -6,24 +6,26 @@ RSpec.describe Types::MutationType do
       @user = create(:user, password: "password")
     end
 
-    it "should log in" do
-      mutation = (
-        %(mutation {
-          signInUser(input: {
-            email: "#{@user.email}"
-            password: "password"
-          }) {
-            token
-            user {
-              id
-            }
-          }
-        })
-      )
+    def perform(args = {})
+      Mutations::SignInUser.new(object: nil, context: { session: {} }).resolve(args)
+    end
 
-      result = SearchAndRescueApiSchema.execute(mutation).as_json['data']['signInUser']
-      expect(result["token"].present?).to be(true)
-      expect(result["user"]["id"].to_i).to be(@user.id)
+    it "should log in with valid credentials" do
+      result = perform(email: @user.email, password: "password")
+      expect(result[:token].present?).to be(true)
+      expect(result[:user][:id].to_i).to be(@user.id)
+    end
+
+    it "should not log in with invalid email" do
+      result = perform(email: "user@email.com", password: "password")
+      expect(result[:token].present?).to be(false)
+      expect(result[:error]).to eq("Email or password is incorrect")
+    end
+
+    it "should not log in with invalid password" do
+      result = perform(email: @user.email, password: "passwOrd")
+      expect(result[:token].present?).to be(false)
+      expect(result[:error]).to eq("Email or password is incorrect")
     end
   end
 end
