f8d071a33bb278000d7e0699d4272011fa7869e7
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-11-19 12:12:10 -0700

    Add message for invalid login token storage into session

--- app/graphql/mutations/sign_in_user.rb
+++ app/graphql/mutations/sign_in_user.rb
@@ -8,12 +8,13 @@ class Mutations::SignInUser < Mutations::BaseMutation
   def resolve(email:, password:)
     user = User.find_by(email: email)
 
-    return unless user
-    return unless user.authenticate(password)
+    return {error: "Email or password is incorrect" } unless user
+    return {error: "Email or password is incorrect" } unless user.authenticate(password)
 
     crypt = ActiveSupport::MessageEncryptor.new(Rails.application.credentials.secret_key_base.byteslice(0..31))
     token = crypt.encrypt_and_sign("user-id:#{ user.id }")
 
+    context[:session][:token] = token
     { user: user, token: token }
   end
 end
